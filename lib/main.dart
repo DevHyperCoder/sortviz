39cefbbfaf2bd5158bdc81195642433a68b3f1ce
DevHyperCoder <devan@devhypercoder.com>
2022-03-26 15:05:47 +0530

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SortViz',
       theme: ThemeData(
         primarySwatch: Colors.blue,
